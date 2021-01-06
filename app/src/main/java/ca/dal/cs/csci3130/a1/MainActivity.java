9cafb355e4877fce072a39a67aa781fc880fbf8a
Masud Rahman <masud.rahman@dal.ca>
2021-01-05 21:38:58 -0400

    Base version of A1

---
+++ app/src/main/java/ca/dal/cs/csci3130/a1/MainActivity.java
@@ -0,0 +1,14 @@
+package ca.dal.cs.csci3130.a1;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
